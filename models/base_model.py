691dda331711a35b3d473cf409eaff589f8596d8
wilstermanz <wilstermanz@gmail.com>
2022-09-28 13:36:52 -0500

    Added public instance methods to base_model

--- models/base_model.py
+++ models/base_model.py
@@ -28,4 +28,23 @@ class BaseModel:
         """
         Override the string to print [<class name>] (<self.id>) <self.__dict__>
         """
-        return("[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__))
+        return "[{}] ({}) {}".format(__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
+        class_dict = self.__dict__
+        class_dict["__class__"] = self.__class__.__name__
+        class_dict["created_at"] = self.created_at.isoformat()
+        class_dict["updated_at"] = self.updated_at.isoformat()
+        return class_dict
